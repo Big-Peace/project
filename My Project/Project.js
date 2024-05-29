61c2d030af4fd69bf0736340d8ec26b602fb9a39
Mustapha Abdulsalam Opeyemi Onifade <124995207+Big-Peace@users.noreply.github.com>
2024-05-29 21:29:45 +0100

    Add files via upload

---
+++ My Project/Project.js
@@ -0,0 +1,9 @@
+// Add javascript code here for interactivity (e.g., form validation, dynamic content loading)
+// Example: Handle form submission
+
+document.getElementById('contactform').addEventListener('submit', function(event)
+{
+event.preventDefault();
+
+alert('Thanks for your submission!');
+});
